9c9d87f93fc3cee2272d537da8ff9d93d2b8c546
Shivam Kamboj Dattana <38610159+Sechunt3r@users.noreply.github.com>
2019-08-06 14:01:18 +0530

    Add files via upload

---
+++ shooter.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import webbrowser as wb
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("W3bSh00t3r")
+print(ascii_banner)
+print("\t Modified By Sechunt3r\n")
+print("[+] USAGE: python3 shooter.py hackerone.com \n")
+domain=input("Enter the domain: ")
+
+wb.open_new_tab("http://www.tcpiputils.com/browse/domain/" +domain)
+
+wb.open_new_tab("http://toolbar.netcraft.com/site_report?url=" +domain)
+
+wb.open_new_tab("https://www.shodan.io/search?query=" +domain)
+
+wb.open_new_tab("https://www.censys.io/ipv4?q=" +domain)
+
+wb.open_new_tab("https://www.google.ca/search?q=site:zone-h.org +" +domain)
+
+wb.open_new_tab("https://securityheaders.io/?q=" +domain)
+
+wb.open_new_tab("https://www.ssllabs.com/ssltest/analyze.html?d=" +domain)
+
+wb.open_new_tab("https://www.threatcrowd.org/domain.php?domain=" +domain)
+
+wb.open_new_tab("https://www.zoomeye.org/searchResult/bugs?q=" +domain)
+
+wb.open_new_tab("https://securitytrails.com/domain/" +domain +"/dns")
+
+wb.open_new_tab("https://web.archive.org/web/*/" +domain)
+
+wb.open_new_tab("http://viewdns.info/reversewhois/?q=" +domain)
+
+wb.open_new_tab("https://www.punkspider.org/#searchkey=url&searchvalue=" +domain+ "&pagenumber=1&filterType=or")
+
+# Subdomain Enumeration
+
+wb.open_new_tab("https://www.virustotal.com/gui/domain/" +domain+ "/relations")
+
+wb.open_new_tab("https://findsubdomains.com/subdomains-of/" +domain)
+
+wb.open_new_tab("https://dns.bufferover.run/dns?q=" +domain)
+
+wb.open_new_tab("https://www.dnsdb.org/" +domain+ "/")
+
+wb.open_new_tab("http://api.hackertarget.com/hostsearch/?q=" +domain)
+
+wb.open_new_tab("https://crt.sh/?q="+domain +".%25")
+
+# Find subdomains through Dorking
+
+wb.open_new_tab("https://www.google.com/search?q=site:*." +domain)
+
+wb.open_new_tab("https://www.google.com/search?q=site:*.*." +domain)
+
+# SQL Dorks
+
+wb.open_new_tab("https://www.google.com/search?q=site:" +domain+ "+username+OR+password+OR+login+OR+root+OR+admin")
+
+wb.open_new_tab("https://www.google.com/search?q=site:" +domain+ "+inurl:shell+OR+inurl:backdoor+OR+inurl:wso+OR+inurl:cmd+OR+shadow+OR+passwd+OR+boot.ini+OR+inurl:backdoor")
+
+wb.open_new_tab("https://www.google.com/search?q=site:" +domain+ "+inurl:readme+OR+inurl:license+OR+inurl:install+OR+inurl:setup+OR+inurl:config")
+
+wb.open_new_tab("https://www.google.com/search?q=site:" +domain+ "+inurl:wp-+OR+inurl:plugin+OR+inurl:upload+OR+inurl:download")
+
+wb.open_new_tab("https://www.google.com/search?q=site:" +domain+ "+inurl:redir+OR+inurl:url+OR+inurl:redirect+OR+inurl:return+OR+inurl:src=http+OR+inurl:r=http")
+
+wb.open_new_tab("https://www.google.com/search?q=site:" +domain+ "+ext:cgi+OR+ext:php+OR+ext:asp+OR+ext:aspx+OR+ext:jsp+OR+ext:jspx+OR+ext:swf+OR+ext:fla+OR+ext:xml")
+
+wb.open_new_tab("https://www.google.com/search?q=site:" +domain+ "+ext:doc+OR+ext:docx+OR+ext:csv+OR+ext:pdf+OR+ext:txt+OR+ext:log+OR+ext:bak")
+
+wb.open_new_tab("https://www.google.com/search?q=site:" +domain+ "+ext:action+OR+struts")
+
+wb.open_new_tab("https://www.google.com/search?q=site:pastebin.com+" +domain)
+
+wb.open_new_tab("https://www.google.com/search?q=site:linkedin.com+employees+" +domain)
+
+print("\n Successfully Completed!!")
+ 
+
+ 
+
+
